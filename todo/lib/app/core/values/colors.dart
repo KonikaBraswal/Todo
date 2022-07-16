099f2f6a5d55589484fb347b2927b05c6d88054f
Konika Braswal <62294761+KonikaBraswal@users.noreply.github.com>
2022-07-16 22:16:16 +0530

    upload

---
+++ todo/lib/app/core/values/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const purple=Color.fromARGB(255, 86, 73, 255);
+const pink=Color.fromARGB(255, 243, 145, 238);
+const darkPink=Color.fromARGB(255, 119, 33, 99);
+const green=Color.fromARGB(255, 12, 254, 64);
+const yellow=Color.fromARGB(255, 199, 167, 8);
+const lightBlue=Color.fromARGB(255, 13, 181, 232);
+const royal=Color.fromARGB(255, 13, 34, 192);
